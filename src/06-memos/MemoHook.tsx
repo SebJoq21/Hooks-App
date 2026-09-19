e16fb6190d13ff3a16f1bb5b1745ccfd759a461a
SEBASTIAN <sebastian.chicata@gmail.com>
2026-09-18 19:30:05 -0500

    Creacion de los componentes del MemoHook

--- src/06-memos/MemoHook.tsx
+++ src/06-memos/MemoHook.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react"
+import { MyTitle } from "./ui/MyTitle"
+import { MySubTitle } from "./ui/MySubTitle"
 
 export const MemoHook = () => {
-  return (
+  const [title, setTitle] = useState('hola')
+  const [subTitle, setSubTitle] = useState('Mundo')
+
+    return (
     <div className="bg-gradient flex flex-col gap-4">
-        <h1 className="text-2xl font-thin text-white">
-            MemoApp
-        </h1>
+        <h1 className="text-2xl font-thin text-white">MemoApp</h1>
 
-        <h1>Mi Título</h1>
+        <MyTitle title={title}/>
+        <MySubTitle subTitle={subTitle}/>
         
-        <h6>Mi Subtítulo</h6>
-
+        
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
             Cambiar Título
         </button>
---
+++ src/06-memos/ui/MySubTitle.tsx
@@ -0,0 +1,16 @@
+interface Props {
+    subTitle: string
+}
+
+export const MySubTitle = ( {subTitle}: Props) => {
+
+  console.log('MySubTitle re-render')
+
+  return (
+    <>
+        <h6 className="text-2xl">{subTitle}</h6>
+
+        <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">Llamar funcion</button>
+    </>
+  )
+}
