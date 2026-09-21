5c35a86a3a2ff4388d7d66418c9a09437f3d9346
SEBASTIAN <sebastian.chicata@gmail.com>
2026-09-21 13:04:22 -0500

    Uso de la librería de Sonner para las notificaciones

--- src/07-useOptimistic/InstagromApp.tsx
+++ src/07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,6 @@
 import { useOptimistic, useState, useTransition } from 'react';
+import { toast } from 'sonner'
+
 
 interface Comment {
   id: number;
@@ -38,10 +40,22 @@ export const InstagromApp = () => {
       // Simular la petición HTTP
       await new Promise( resolve => setTimeout(resolve, 3000))
   
-      setComments(prev => [...prev, {
-          id: new Date().getTime(),
-          text: messageText,
-      }])
+      // setComments(prev => [...prev, {
+      //     id: new Date().getTime(),
+      //     text: messageText,
+      // }])
+
+      //! Este sería el código para revertir el proceso
+      setComments((prev) => prev)
+      toast('Error al agregar el comentario', {
+        description: 'Intente nuevamente',
+        duration: 10_000,
+        position: 'top-right',
+        action: {
+          label: 'Cerrar',
+          onClick: () => toast.dismiss()
+        }
+      })
     })
 
   };
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner'
+
 
 //import { HooksApp } from './HooksApp'
 //import { TrafficLight } from './01-useState/TrafficLight'
@@ -17,6 +19,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster/>
     {/*<HooksApp/>*/}
     {/*<TrafficLight/>*/}
     {/*<TrafficLightWithEffect/>*/}
