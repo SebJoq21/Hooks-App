19517c496db3120e7ff92013533e837ef2413c0c
SEBASTIAN <sebastian.chicata@gmail.com>
2026-09-21 13:31:05 -0500

    Uso del use API y el use-suspense

---
+++ src/08-use-suspense/api/get-user.action.ts
@@ -0,0 +1,21 @@
+
+export interface User {
+    id: number, 
+    name: string,
+    location: string, 
+    role: string
+}
+
+export const getUserAction = async( id: number ) => {
+    console.log('Función llamada')
+    await new Promise((res) => setTimeout(res, 2000))
+
+    console.log('Función resolvió')
+
+    return {
+        id: id,
+        name: 'Sebastian Chicata',
+        location: 'Lima, Peru',
+        role: 'Desarrollador Backend'
+    }
+}
