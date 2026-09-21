c168a18363daad3678a476c9be00670f69bbdf68
SEBASTIAN <sebastian.chicata@gmail.com>
2026-09-20 22:25:48 -0500

    Uso del useTransition

--- src/07-useOptimistic/InstagromApp.tsx
+++ src/07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,4 @@
-import { useOptimistic, useState } from 'react';
+import { useOptimistic, useState, useTransition } from 'react';
 
 interface Comment {
   id: number;
@@ -6,7 +6,12 @@ interface Comment {
   optimistic?: boolean;
 }
 
+let lastId = 2
+
 export const InstagromApp = () => {
+  
+  const [ isPending, startTransition ] = useTransition()
+  
   const [comments, setComments] = useState<Comment[]>([
     { id: 1, text: '¡Gran foto!' },
     { id: 2, text: 'Me encanta 🧡' },
@@ -14,9 +19,9 @@ export const InstagromApp = () => {
 
   const [ optimisticComments, addOptimisticComment ] = useOptimistic( comments, 
     ( currentComments, newCommentText: string) => {
-      
+      lastId++
       return [...currentComments, {
-        id: new Date().getTime(),
+        id: lastId,
         text: newCommentText,
         optimistic: true
       }]
@@ -29,13 +34,16 @@ export const InstagromApp = () => {
 
     addOptimisticComment(messageText)
 
-    // Simular la petición HTTP
-    await new Promise( resolve => setTimeout(resolve, 3000))
+    startTransition( async() => {
+      // Simular la petición HTTP
+      await new Promise( resolve => setTimeout(resolve, 3000))
+  
+      setComments(prev => [...prev, {
+          id: new Date().getTime(),
+          text: messageText,
+      }])
+    })
 
-    setComments(prev => [...prev, {
-        id: new Date().getTime(),
-        text: messageText,
-    }])
   };
 
   return (
@@ -81,7 +89,7 @@ export const InstagromApp = () => {
         />
         <button
           type="submit"
-          disabled={false}
+          disabled={isPending}
           className="bg-blue-500 text-white p-2 rounded-md w-full"
         >
           Enviar
